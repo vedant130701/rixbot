773bc96ba3fad2f3cd3630dc7b59b2d43b6fa8e6
Vedant Rishi Das <vedantdas130701@gmail.com>
2022-06-15 10:01:59 +0530

    latest entity extraction to be used in custom action

--- actions/actions.py
+++ actions/actions.py
@@ -23,11 +23,19 @@ class ActionTellJoke(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
+        
         joke = ""
+        try:
+            entityVal = tracker.latest_message['entities'][0]['value']
+        except:
+            entityVal = ""
+        # entityVal = tracker.get_latest_entity_values('str')
+        # entityVal = tracker.get_slot("joke_type")
+        print(type(entityVal))
+        print(entityVal)
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         nest_asyncio.apply()
-        joke = asyncio.run(print_joke())
+        joke = asyncio.run(print_joke(entityVal))
         dispatcher.utter_message(text=joke)
 
         return []
--- scripts/joke.py
+++ scripts/joke.py
@@ -1,9 +1,14 @@
 from jokeapi import Jokes # Import the Jokes class
 import asyncio
 
-async def print_joke():
+async def print_joke(entityVal):
     j = await Jokes()  # Initialise the class
-    joke = await j.get_joke()  # Retrieve a random joke
+    print(entityVal)
+    if entityVal == "":
+        joke = await j.get_joke()
+    else:
+        joke = await j.get_joke(category = [entityVal])
+    # joke = await j.get_joke()  # Retrieve a random joke
     if joke["type"] == "single": # Print the joke
         return joke["joke"]
     else:
